11934708ed9bf8a63d220d9eaa3d59d3f118b62e
Miguel Nunes <miguel.nunes@heiwa.eti.br>
2022-07-07 02:07:58 -0300

    Fixing syntax

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function compute()
     var year = new Date().getFullYear()+parseInt(years);
     
     if(this.validatePrincipal()) {
-        document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark><br>at an interest rate of <mark>" + rate + "%</mark>,<br>You will receive an amount of <mark>" + interest + ",</mark><br>in the year <mark>" + year + "</mark>";  
+        document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br>at an interest rate of <mark>" + rate + "%</mark>,<br>You will receive an amount of <mark>" + interest + ",</mark><br>in the year <mark>" + year + "</mark>";  
     }
     
 }
